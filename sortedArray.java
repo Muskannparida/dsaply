b3e657ec9cdfbc065c0adb5dde547e3a78b3b692
siyasahoo <paridamuskann@gmail.com>
2026-08-11 19:30:12 +0530

    Added sortedArray.java

---
+++ sortedArray.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class sortedArray {
+    static boolean isSorted(int[]arr){
+        boolean check = true;
+
+        for(int i = 1;i<arr.length;i++){
+            if(arr[i] < arr[i-1]){
+                check = false;
+                break;
+            }
+        }
+        return check;
+         
+    }
+    public static void main(String[] args) {
+        int[]arr = {1 ,4  ,6 , 8};
+        System.out.println("is it a sorted array ? ---"+isSorted(arr));
+
+    }
+
+}

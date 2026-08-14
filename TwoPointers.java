6ea82a3a69e7ca2053c01355481633baabe61386
siyasahoo <paridamuskann@gmail.com>
2026-08-14 16:09:51 +0530

    Added TwoPointer.java

---
+++ TwoPointers.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class TwoPointers {
+
+    static void printArray(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    static void sortZeroesAndOnes(int[] arr) {
+        int n = arr.length;
+        int zeroes = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 0) {
+                zeroes++;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (i < zeroes) {
+                arr[i] = 0;
+            } else {
+                arr[i] = 1;
+            }
+        }
+    }   // closes sortZeroesAndOnes()
+
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter the size of array: ");
+    int n = sc.nextInt();
+
+    int[] arr = new int[n];
+
+    System.out.println("Enter " + n + " elements (only 0 and 1):");
+    for (int i = 0; i < n; i++) {
+        arr[i] = sc.nextInt();
+    }
+
+    System.out.println("Original Array:");
+    printArray(arr);
+
+    sortZeroesAndOnes(arr);
+
+    System.out.println("Sorted Array:");
+    printArray(arr);
+
+    sc.close();
+}
+}

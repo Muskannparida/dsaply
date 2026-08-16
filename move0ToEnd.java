6d4a3b1671a4850dd31c54929db301251f172e0e
siyasahoo <paridamuskann@gmail.com>
2026-08-16 21:24:14 +0530

    Added move0ToEnd

---
+++ move0ToEnd.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+public class move0ToEnd {
+ static void pushZerosToEnd(int[] arr) {
+        int j = 0; 
+        // Move non-zero elements to the front
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                arr[j++] = arr[i];
+            }
+        }
+
+        // Fill the remaining positions with zeros
+        while (j < arr.length) {
+            arr[j++] = 0;
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0, 0, 0, 9, 0};
+        pushZerosToEnd(arr);
+
+}
+}

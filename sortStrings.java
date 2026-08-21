843d028fbf5eda0f15054e59422874c24c9b0e28
siyasahoo <paridamuskann@gmail.com>
2026-08-21 20:58:15 +0530

    Added sortStrings.java

---
+++ sortStrings.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Collections;
+public class sortStrings {
+    static void sortArrayList(ArrayList<String>s1) {
+        Collections.sort(s1);
+    }
+    public static void main(String[] args) {
+        ArrayList<String> s1 = new ArrayList<>();
+        s1.add("Muskan");
+        s1.add("eva");
+        s1.add("Ram");
+        s1.add("manvi");
+        System.out.println("Original list :"+s1);
+        sortArrayList(s1);
+        System.out.println("Sorted Array : "+s1);
+         
+   
+    }
+}

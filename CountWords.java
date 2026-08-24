abd5128663aa371cfbec2ce3358720cf274057f0
siyasahoo <paridamuskann@gmail.com>
2026-08-24 19:51:00 +0530

    Added CountWords.java

---
+++ CountWords.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class CountWords {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        if (str.trim().isEmpty()) {
+            System.out.println(0);
+        } else {
+            String[] words = str.trim().split("\\s+");
+            System.out.println(words.length);
+        }
+        sc.close();
+    }
+}
+
+

534bddb2d5f2d1cca4608d72f852cbfc9aa15592
siyasahoo <paridamuskann@gmail.com>
2026-08-24 19:58:49 +0530

    Added StringDuplicate.java

--- StringDuplicate.java
+++ StringDuplicate.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 public class StringDuplicate {
     static String removeDuplicates(String str) {
-        String ans = "";
+        String answer = "";
 
         for (int i = 0; i < str.length(); i++) {
             if (i == 0 || str.charAt(i) != str.charAt(i - 1)) {
-                ans += str.charAt(i);
+                answer += str.charAt(i);
             }
         }
 
-        return ans;
+        return answer;
     }
 
     public static void main(String[] args) {
